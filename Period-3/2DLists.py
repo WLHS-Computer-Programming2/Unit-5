b40bef4965cdbe644b518ba1234b8c7c1ef64c75
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-26 13:19:44 -0800

    new

--- Period-3/2DLists.py
+++ Period-3/2DLists.py
@@ -15,10 +15,23 @@ my_grid = [[-3,2,0,-1,7,8,3,9],
             [2,7,7,3,3,-5,7,8],
             [9,-3,7,7,7,-5,3,9]
 ]
-print(my_grid[7][2:5]) # prints 7,7,7 from last row
-print(my_grid[-1][2:5]) # prints 7,7,7 from last row
-print(my_grid[1:3]) # prints rows 1 and 2
+#print(my_grid[7][2:5]) # prints 7,7,7 from last row
+#print(my_grid[-1][2:5]) # prints 7,7,7 from last row
+#print(my_grid[1:3]) # prints rows 1 and 2
 
 # print all of the data from the third column
 # should print [0,-3,3,7,7,3,7,7]
 target = [0,-3,3,7,7,3,7,7]
+column_three = []
+
+for i in range(len(my_grid)):
+    column_three.append(my_grid[i][2])
+assert column_three == target
+
+# try to do this with enumeration
+for index,item in enumerate(my_grid):
+    print(my_grid[index][2])
+
+my_grid.append([100,100,100,100])
+for item in my_grid:
+    print(item)
