c45b9e64e5176f29212ce3928bb5c5e5a13fa503
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-26 12:49:24 -0800

    pre-example 1 push

--- Period-3/2DLists.py
+++ Period-3/2DLists.py
@@ -6,8 +6,7 @@ Topic: 2D Lists
 '''
 
 # 2D lists are like a grid
-my_grid = [
-            [-3,2,0,-1,7,8,3,9],
+my_grid = [[-3,2,0,-1,7,8,3,9],
             [5,9,-3,-5,3,9,0,-9],
             [2,0,3,3,7,8,3,2],
             [9,-3,7,7,3,9,0,9],
@@ -16,3 +15,8 @@ my_grid = [
             [2,7,7,3,3,-5,7,8],
             [9,-3,7,7,7,-5,3,9]
 ]
+print(my_grid[7][2:5]) # prints 7,7,7 from last row
+print(my_grid[-1][2:5]) # prints 7,7,7 from last row
+print(my_grid[1:3]) # prints rows 1 and 2
+
+# print all of the data from the third column
