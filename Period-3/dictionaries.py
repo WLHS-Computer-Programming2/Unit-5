efc83212d213a6ec826b376f1068e3ca0fba68e9
smithbwlhs <smithb@wlwv.k12.or.us>
2024-03-07 12:15:01 -0800

    initial

---
+++ Period-3/dictionaries.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+    'jen':'rust'
+}
+# dictionaries are made of keys and values
+# the key jen has a value of python
