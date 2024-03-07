64501490e1f7645d650891eb1362cb3ed1ea024d
smithbwlhs <smithb@wlwv.k12.or.us>
2024-03-07 12:31:23 -0800

    updated

--- Period-3/dictionaries.py
+++ Period-3/dictionaries.py
@@ -7,3 +7,23 @@ favorite_languages = {
 }
 # dictionaries are made of keys and values
 # the key jen has a value of python
+print(favorite_languages)
+print(f"Jen's favorite language is {favorite_languages['jen']}")
+favorite_languages['jen'] = 'javascript'
+print(f"Jen's favorite language is {favorite_languages['jen']}")
+# if you have a duplicate key, only the last one is used
+# but the order doesn't change. 
+
+# print dictionaries the Pythonic way
+
+# to print keys and values the template is
+# for key,value in dictionary_name.items()
+
+for person,language in favorite_languages.items():
+    print(f"The favorite language of {person.title()} is {language.title()}")
+
+# to print keys only the template is
+# for key in dictionary_name.keys()
+
+for person in favorite_languages.keys():
+    print(person.title())
