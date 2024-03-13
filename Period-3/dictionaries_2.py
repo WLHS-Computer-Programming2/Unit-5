23833c9ecffe5cb275f171651cf6c42ea104cf62
smithbwlhs <smithb@wlwv.k12.or.us>
2024-03-13 13:19:55 -0700

    initial

--- Period-3/dictionaries_2.py
+++ Period-3/dictionaries_2.py
@@ -1,17 +1,23 @@
 users = {
     'aeinstein':{
-        'first':'albert',
-        'last':'einstein',
+        'first name':'albert',
+        'last name':'einstein',
         'location':'princeton'
     },
     'mcurie':{
-        'first':'marie',
-        'last':'curie',
+        'first name':'marie',
+        'last name':'curie',
         'location':'paris'
     },
-    'rfeyman':{
-        'first':'richard',
-        'last': 'feyman',
+    'rfeynman':{
+        'first name':'richard',
+        'last name': 'feyman',
         'location': 'cal tech'
     }
 }
+#   key       value(dict)
+for username,user_info in users.items():
+    print(f"The user with username {username} has a:")
+    #   key  value
+    for info,value in user_info.items():
+        print(f"\t{info} of {value.title()}")
---
+++ Period-3/hw_review_for_test.py
@@ -0,0 +1,20 @@
+# Unit 4 HW 1
+
+'''Create a function called list_of_cubes(start). That function should make, and return, a list of the first 10 cubes beginning with start using list comprehension. Then print the values from each of those lists to the console. For example, list_of_cubes(5) would return
+
+[125, 216, 343, 512, 729, 1000, 1331, 1728, 2197, 2744]'''
+def list_of_cubes(start:int)->list:
+    cubes = []
+    return None
+
+
+'''
+Create a function called pizza_modifier(topping_list, new_topping) that allows a user to add a new topping to a list of toppings. The function should print the current toppings and print the toppings with the new topping added.'''
+
+
+def main():
+    list_of_cubes()
+
+
+if __name__ == "__main__":
+    main()
