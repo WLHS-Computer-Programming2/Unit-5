0ca4f3a6af6406f5bac20ff806720f39439277a1
smithbwlhs <smithb@wlwv.k12.or.us>
2024-03-13 13:32:53 -0700

    updated

--- Period-3/hw_review_for_test.py
+++ Period-3/hw_review_for_test.py
@@ -4,16 +4,24 @@
 
 [125, 216, 343, 512, 729, 1000, 1331, 1728, 2197, 2744]'''
 def list_of_cubes(start:int)->list:
-    cubes = []
-    return None
+    #                              5      15
+    cubes = [i**3 for i in range(start,start+10)]
+    return cubes
 
 
 '''
 Create a function called pizza_modifier(topping_list, new_topping) that allows a user to add a new topping to a list of toppings. The function should print the current toppings and print the toppings with the new topping added.'''
-
+def pizza_modifier(topping_list,new_topping):
+    print(topping_list)
+    topping_list.append(new_topping)
+    print(topping_list)
+    return None
 
 def main():
-    list_of_cubes()
+    print(list_of_cubes(5))
+    list_of_toppings = ['cheese','ham']
+    pizza_modifier(list_of_toppings[:],'pineapple') #[:] passes a copy of the list
+    print(list_of_toppings)
 
 
 if __name__ == "__main__":
---
+++ Period-3/test_topics.md
@@ -0,0 +1,19 @@
+# Units 1 - 5 test
+
+* Structure
+    * no more than 15 mc questions on paper
+    * a free response programming question on computer
+
+* Topics
+    * Writing functions with and without parameters and with and withouth return values
+    * Try/Except
+    * Slicing strings, including reversing with slicing
+    * Iterating strings the Pythonic way
+    * Using the in keyword
+    * Mutability vs immutability - think lists vs. tuples
+    * Accessing/modifiying tuples and lists
+    * Using list methods
+    * Iterating through 2D arrays and dictionaries
+    * Modifying 2D arrays and dictionaries
+    * Packing/unpacking
+
