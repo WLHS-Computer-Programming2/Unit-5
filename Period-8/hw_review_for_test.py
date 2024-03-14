0b1992535c8056cc9df5f601307b81745f829586
smithbwlhs <smithb@wlwv.k12.or.us>
2024-03-14 14:34:44 -0700

    initial

---
+++ Period-8/hw_review_for_test.py
@@ -0,0 +1,15 @@
+# Unit 4 HW 1
+
+'''Create a function called list_of_cubes(start). That function should make, and return, a list of the first 10 cubes beginning with start using list comprehension. Then print the values from each of those lists to the console. For example, list_of_cubes(5) would return
+
+[125, 216, 343, 512, 729, 1000, 1331, 1728, 2197, 2744]'''
+
+
+'''
+Create a function called pizza_modifier(topping_list, new_topping) that allows a user to add a new topping to a list of toppings. The function should print the current toppings and print the toppings with the new topping added.'''
+
+def main():
+   print()
+
+if __name__ == "__main__":
+    main()
